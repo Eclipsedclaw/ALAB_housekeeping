41ea86b551b70e09de30628be320c9f591c13d56
Pi4 <pi4@raspi.com>
2025-04-02 17:59:51 +0100

    added 5 trials if failed

--- MHADC/MHADC2database.py
+++ MHADC/MHADC2database.py
@@ -14,22 +14,22 @@ def convert_RTD_ADC(x, offset):
         # Validate input x
         x_float = float(x)
     except ValueError:
-        #print("Error: Input 'x' is not a valid numeric value.")
-        return False
+        print("Error: Input 'x' is not a valid numeric value.")
+        return None
 
     try:
         # Validate input offset
         offset_float = float(offset)
     except ValueError:
-        #print("Error: Input 'offset' is not a valid numeric value.")
-        return False
+        print("Error: Input 'offset' is not a valid numeric value.")
+        return None
 
     # Perform the calculations
     try:
         L_V = x_float * (3.3 / 2715.0)
-        if(x==2715):
+        if(x>=2715):
             print("Open circuit!")
-            return False
+            return None
         else:
             L_R = L_V * 1000. / (3.3 - L_V)
             L_tmp = -(math.sqrt(17.59246 - 0.00232 * L_R) - 3.908) / 0.00116
@@ -38,7 +38,7 @@ def convert_RTD_ADC(x, offset):
             return round(float(L_correction), 2)    # Round to 2 digits
     except Exception as e:
         print(f"Error during data querying")
-        return False
+        return None
 
 
 def complete_path(text, state):
@@ -115,37 +115,47 @@ baud_rate = 9600
 # TODO: Current only take readout command a, if we have further comamnd, need to implement here
 command = "a"
 
-try:
-    while True:
-        ser = serial.Serial(port, baud_rate)
-        ser.write(command.encode())
 
-        response = ser.readline().decode().strip()
-        chunks = response.split('_')
+max_retries = 5  # Maximum number of retries
+retry_delay = 5   # Delay between retries in seconds
 
-        values = [int(chunks[i]) for i in range(1, len(chunks), 2)]
-        offset = [-2, -4, -2, -5, -4, -2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        # channel definition, currently 1 means temperature sensor that need conversion, 0 means raw digital output
-        channel_def = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        hubADC_output = []
-        for n in range(len(values)):
-            if channel_def[n]==1:
-                hubADC_output.append(convert_RTD_ADC(values[n], offset[n]))
-            if channel_def[n]==0:
-                hubADC_output.append(values[n])
+for attempt in range(max_retries):
+
+    try:
+        while True:
+            ser = serial.Serial(port, baud_rate)
+            ser.write(command.encode())
+
+            response = ser.readline().decode().strip()
+            print("raw readout is: ", response)
+            chunks = response.split('_')
+
+            values = [int(chunks[i]) for i in range(1, len(chunks), 2)]
+            offset = [-2, -4, -2, -5, -4, -2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            # channel definition, currently 1 means temperature sensor that need conversion, 0 means raw digital output
+            channel_def = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            hubADC_output = []
+            for n in range(len(values)):
+                if channel_def[n]==1:
+                    hubADC_output.append(convert_RTD_ADC(values[n], offset[n]))
+                if channel_def[n]==0:
+                    hubADC_output.append(values[n])
 
 
-        # MHADC 32 channels
-        ADCchannel_name = [f"Ch{i+1}" for i in range(len(hubADC_output))]
-        ADCchannel_type = ["float" if ch_def == 1 else "int" for ch_def in channel_def]
-        # insert
-        cursor = Cursor(host=os.environ.get('LAZYINS_HOST'), port=os.environ.get('LAZYINS_PORT'), user=os.environ.get('LAZYINS_USER'), passwd=os.environ.get('LAZYINS_PASSWD'), db_name = db_name, table_name = chart_name)
-        if(all(element is None for element in hubADC_output)):
-            print("ALL MKS readout shows NONE! Check communication connection!")
-        else:
-            cursor.setup(ADCchannel_name, types = ADCchannel_type)
-            cursor.register(hubADC_output)
-            print("readout MHADC as: ", hubADC_output)
-        time.sleep(1)  # Delay for 0.1 second
-except KeyboardInterrupt:
-    print("Process interrupted by the user.")
+            # MHADC 32 channels
+            ADCchannel_name = [f"Ch{i+1}" for i in range(len(hubADC_output))]
+            ADCchannel_type = ["float" if ch_def == 1 else "int" for ch_def in channel_def]
+            # insert
+            cursor = Cursor(host=os.environ.get('LAZYINS_HOST'), port=os.environ.get('LAZYINS_PORT'), user=os.environ.get('LAZYINS_USER'), passwd=os.environ.get('LAZYINS_PASSWD'), db_name = db_name, table_name = chart_name)
+            if(all(element is None for element in hubADC_output)):
+                print("ALL MKS readout shows NONE! Check communication connection!")
+            else:
+                cursor.setup(ADCchannel_name, types = ADCchannel_type)
+                cursor.register(hubADC_output)
+                print("\nreadout MHADC as: ", hubADC_output)
+            time.sleep(1)  # Delay for 0.1 second
+    except Exception as e:
+        print(f"Attempt {attempt + 1} failed: {str(e)}")
+        if attempt == max_retries - 1:
+            raise  # Re-raise the last exception if all retries failed
+        time.sleep(retry_delay)
